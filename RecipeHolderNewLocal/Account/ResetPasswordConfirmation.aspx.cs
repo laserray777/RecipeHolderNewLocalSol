aba2c2fcd7104f45fc676226a3e4b6c10bf734b5
Raymond Gregowicz <laserray777@lasercrops.com>
2017-12-07 14:18:14 -0600

    Add project files.

---
+++ RecipeHolderNewLocal/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace RecipeHolderNewLocal.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ RecipeHolderNewLocal/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecipeHolderNewLocal.Startup))]
+namespace RecipeHolderNewLocal
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
